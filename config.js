1cd83b4d16841730aba8ccb353a3f6e22efeffdd
knieeeee <88036713+knieeeee@users.noreply.github.com>
2022-03-05 15:33:40 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Dương à!',
+    introDesc: 'b_autiful hoàn chỉnh hơn khi có e',
+    btnIntro: 'hihi',
+    title: '1 phút mơ màng 😘',
+    desc: 'Trăm năm yêu nàng :3333333',
+    btnYes: 'hay quá :33333333',
+    btnNo: 'Xàm quá:)',
+    question:'Tại sao hay hãy nói cho tớ :>',
+    btnReply: 'Hãy nói điều cậu muốn nói :>',
+    reply: 'Yêu thì thích thôi :33',
+    mess: 'lớp diu :33',
+    messDesc: 'Hãy đợi tớ đỗ cấp 3 nha Dương <3.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' 
+  
+    
+}
